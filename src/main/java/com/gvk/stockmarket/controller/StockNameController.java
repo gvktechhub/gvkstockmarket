733e2710b95fbef252ef333c5ea60cbf57569b49
DELL <DELL@DESKTOP-9VFNF4E>
2021-05-29 16:56:04 +0530

    removed unnecessary imports

--- src/main/java/com/gvk/stockmarket/controller/StockNameController.java
+++ src/main/java/com/gvk/stockmarket/controller/StockNameController.java
@@ -1,13 +1,11 @@
 package com.gvk.stockmarket.controller;
 
-import java.util.Objects;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.aop.ThrowsAdvice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
