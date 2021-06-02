f5ca18d335040a3d60d0a224ebdf20bed1a4296c
DELL <DELL@DESKTOP-9VFNF4E>
2021-06-02 11:28:57 +0530

    logger added at welcome controller

--- src/main/java/com/gvk/stockmarket/controller/WelcomeController.java
+++ src/main/java/com/gvk/stockmarket/controller/WelcomeController.java
@@ -1,5 +1,7 @@
 package com.gvk.stockmarket.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value="/welcome")
 public class WelcomeController {
+	
+	private static final Logger log = LoggerFactory.getLogger(WelcomeController.class);
 	
 	@GetMapping(value="/index")
 	public String index() {
